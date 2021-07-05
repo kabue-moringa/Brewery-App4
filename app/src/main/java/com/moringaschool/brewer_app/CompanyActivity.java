a08824dedfe4730f2752338e2c7050fecad33be5
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-05 20:04:37 +0300

    Add changes

--- app/src/main/java/com/moringaschool/brewer_app/CompanyActivity.java
+++ app/src/main/java/com/moringaschool/brewer_app/CompanyActivity.java
@@ -7,6 +7,11 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CompanyActivity  extends AppCompatActivity {
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
