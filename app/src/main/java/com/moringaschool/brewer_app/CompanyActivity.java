edba9c5179ced941b18d7a7da3889f7e6672ef93
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-05 22:57:56 +0300

    Add changes

--- app/src/main/java/com/moringaschool/brewer_app/CompanyActivity.java
+++ app/src/main/java/com/moringaschool/brewer_app/CompanyActivity.java
@@ -2,26 +2,33 @@ package com.moringaschool.brewer_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CompanyActivity  extends AppCompatActivity {
-    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
-            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
-            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
-            "Lardo", "Portland City Grill", "Fat Head's Brewery",
-            "Chipotle", "Subway"};
+    private ListView mListView;
+    private String[] companies = new String[] {"Anheuser-Busch InBev.", "Tsingtao Brewery Group.",
+            "Carlsberg", "Groupe Castel", "Molson Coors Brewing", "Heineken",
+            "Yanjing", " China Resources Snow Breweries", " Asahi", "Tsingtao Brewery Group",
+    };
+
     TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company);
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, companies);
+        mListView.setAdapter(adapter);
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         String company = intent.getStringExtra("company");
         textView=(TextView)findViewById(R.id.textView2) ;
-        textView.setText("The leading Breweries");
+        textView.setText("Largest Brewies");
 
 
 //        String company = intent.getStringExtra("location");
