5e00f91f92735ca369f38db894587678b4624b32
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-05 13:54:08 +0300

    Add changes

--- app/src/main/java/com/moringaschool/brewer_app/MainActivity.java
+++ app/src/main/java/com/moringaschool/brewer_app/MainActivity.java
@@ -6,16 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindCompanyButton;
+    EditText name;
+    EditText company;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFindCompanyButton = (Button)findViewById(R.id.findCompanyButton);
+        name=findViewById(R.id.editText);
+        company=findViewById(R.id.login);
 
 
     mFindCompanyButton.setOnClickListener(new View.OnClickListener() {
