fd742a19044652694da07c0942a2a5cbd821515a
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-05 23:45:09 +0300

    Add files

---
+++ app/src/main/java/com/moringaschool/brewer_app/CompanyActivity2.java
@@ -0,0 +1,14 @@
+package com.moringaschool.brewer_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompanyActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company2);
+    }
+}
