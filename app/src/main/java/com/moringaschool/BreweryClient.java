9b1f16d8dba869982e8da0cbe56be0160f3e1abd
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-11 15:51:39 +0300

    Add changes

--- app/src/main/java/com/moringaschool/BreweryClient.java
+++ app/src/main/java/com/moringaschool/BreweryClient.java
@@ -2,20 +2,32 @@ package com.moringaschool;
 
 import com.moringaschool.brewerydb.Constants;
 
+
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.moringaschool.brewerydb.Constants.BREWERYDB_BASE_URL;
 
 public class BreweryClient {
     private static Retrofit.Builder retrofit = null;
 
    public static BreweryApi getClient(){
        if(retrofit == null){
-           retrofit = new Retrofit.Builder();
-           baseUrl(Constants.BREWERYDB_BASE_URL);
+
+                    retrofit = new Retrofit.Builder()
+                                    .baseUrl(BREWERYDB_BASE_URL)
+                                    .addConverterFactory(GsonConverterFactory.create())
+                                  
+
+
 
 
        }
 
     }
+
+
+
 
 
 }
