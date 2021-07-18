c2db180a887765bf1331126ad50a01f528088463
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-18 15:14:13 +0300

    initializw shared prefernces

--- app/src/main/java/com/moringaschool/brewerydb/Constants.java
+++ app/src/main/java/com/moringaschool/brewerydb/Constants.java
@@ -1,5 +1,7 @@
 package com.moringaschool.brewerydb;
 
 public class Constants {
+
     public static final String BREWERYDB_BASE_URL = "https://api.openbrewerydb.org/";
+    public static final String PREFERENCES_LOCATION_KEY = "location";
 }
--- app/src/main/java/com/moringaschool/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/ui/MainActivity.java
@@ -3,7 +3,9 @@ package com.moringaschool.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,37 +13,57 @@ import android.widget.TextView;
 
 import com.moringaschool.brewer_app.CompanyActivity;
 import com.moringaschool.brewer_app.R;
+import com.moringaschool.brewerydb.Constants;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.findCompanyButton) Button mFindRestaurantsButton;;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+//    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.findCompanyButton) Button mFindCompanyButton;;
     @BindView(R.id.textView)
     TextView mAppNameTextView;
-    private Button mFindCompanyButton;
+//    private Button mFindCompanyButton;
     EditText name;
     EditText company;
+    EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFindCompanyButton = (Button)findViewById(R.id.findCompanyButton);
-
         ButterKnife.bind(this);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
 
     mFindCompanyButton.setOnClickListener(new View.OnClickListener() {
+
         @Override
         public void onClick(View v) {
-
-            Intent intent = new Intent(MainActivity.this, CompanyActivity.class);
-           startActivity(intent);
-
+            if(v ==mFindCompanyButton) {
+                String location = mLocationEditText.getText().toString();
+                addToSharedPreferences(location);
+                Intent intent = new Intent(MainActivity.this, CompanyActivity.class);
+                startActivity(intent);
+            }
 
         }
 
+        private void addToSharedPreferences(String location) {
+            mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+        }
+
     });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
     }
 }
 
